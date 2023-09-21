25d9186a2e126bbd0cb9362ddfcbbfac3826571b
EJhagenaar <69298881+EJhagenaar@users.noreply.github.com>
2023-09-21 14:32:22 +0200

    Update functions.py

--- Eopti/functions.py
+++ Eopti/functions.py
@@ -115,7 +115,6 @@ class Eoptimization:
     def loadEdata(self):
         #get consumption
         self.edata=self.getfromInflux('edata')
-        print(self.edata)
         self.edata.index.name='time'
         self.edata.index = self.edata.index.tz_convert(self.influxconfig['timezone'])
         self.edata = self.edata.asfreq('H', fill_value=0.0).sort_index()
